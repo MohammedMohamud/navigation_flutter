649b7ebac181e1bbc28605f6fe6de1e6f887bfae
MohamedMohamud <mnoormohamud17@gmail.com>
2024-01-01 00:49:25 +0530

    Initial commit

---
+++ lib/screens/details_screens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+
+  int counter;
+  DetailsScreen(this.counter);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text("$counter"),),
+    );
+  }
+}
